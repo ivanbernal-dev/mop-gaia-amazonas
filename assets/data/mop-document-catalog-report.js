aeedcef709213c83e7129c1913f22b17380a721a
ivanbernal-dev <ivan.bernal@gaiaamazonas.org>
2026-08-06 18:05:33 -0500

    Actualiza catálogo documental en modo de prueba

---
+++ assets/data/mop-document-catalog-report.js
@@ -0,0 +1,16 @@
+window.MOP_DOCUMENT_CATALOG_REPORT = {
+  "generatedAt": "2026-08-06",
+  "recordsTotal": 106,
+  "catalogVisible": 22,
+  "catalogHidden": 84,
+  "downloadsAuthorized": 0,
+  "previewModeExpected": true,
+  "classificationCounts": {
+    "Pendiente de clasificación": 84,
+    "Interna": 22
+  },
+  "emptyCodeCount": 0,
+  "duplicateCodeGroupCount": 2,
+  "visibleDuplicateCodes": [],
+  "sensitiveValueCount": 0
+};
